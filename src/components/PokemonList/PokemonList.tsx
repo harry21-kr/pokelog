90fa693b66b9232a264e269240ba2ca0d7774297
박원빈 <harry21@kakao.com>
2024-07-02 10:35:34 +0900

    (add) Link to detail page

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -3,7 +3,7 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Image from "next/image";
-import { useEffect } from "react";
+import Link from "next/link";
 
 const PokemonList = () => {
   const { data: pokemonList } = useSuspenseQuery<Pokemon[]>({
@@ -12,25 +12,23 @@ const PokemonList = () => {
       (await axios.get("http://localhost:3000/api/pokemons")).data,
   });
 
-  useEffect(() => {
-    console.log(pokemonList);
-  }, [pokemonList]);
-
   return (
     <ul className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-x-5 gap-y-8">
       {pokemonList.map((pokemon) => (
         <li key={pokemon.id} className="bg-gray-800">
-          <h6>{pokemon.korean_name}</h6>
-          <div className="relative aspect-square">
-            <Image
-              src={pokemon.sprites.front_default}
-              alt={pokemon.name}
-              fill
-              sizes="300px"
-              className="object-cover"
-              priority
-            />
-          </div>
+          <Link href={`/${pokemon.id}`}>
+            <h6>{pokemon.korean_name}</h6>
+            <div className="relative aspect-square">
+              <Image
+                src={pokemon.sprites.front_default}
+                alt={pokemon.name}
+                fill
+                sizes="300px"
+                className="object-cover"
+                priority
+              />
+            </div>
+          </Link>
         </li>
       ))}
     </ul>
