246cb6e6d2b672fecfbc51105493ee17c720992c
박원빈 <harry21@kakao.com>
2024-07-03 17:15:12 +0900

    (fix) build error

--- src/app/api/pokemons/route.ts
+++ src/app/api/pokemons/route.ts
@@ -1,32 +1,9 @@
-import axios from "axios";
+import { getPokemonList } from "@/utils/api/api";
 import { NextResponse } from "next/server";
-
-const TOTAL_POKEMON = 151;
 
 export const GET = async (request: Request) => {
   try {
-    const allPokemonPromises = Array.from(
-      { length: TOTAL_POKEMON },
-      (_, index) => {
-        const id = index + 1;
-        return Promise.all([
-          axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
-          axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
-        ]);
-      }
-    );
-
-    const allPokemonResponses = await Promise.all(allPokemonPromises);
-
-    const allPokemonData = allPokemonResponses.map(
-      ([response, speciesResponse]) => {
-        const koreanName = speciesResponse.data.names.find(
-          (name: any) => name.language.name === "ko"
-        );
-        return { ...response.data, korean_name: koreanName?.name || null };
-      }
-    );
-
+    const allPokemonData = await getPokemonList();
     return NextResponse.json(allPokemonData);
   } catch (error) {
     return NextResponse.json({ error: "Failed to fetch data" });
--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,15 +1,86 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
+
+const TOTAL_POKEMON = 151;
 
 export async function getPokemonList() {
-  const res: AxiosResponse<Pokemon[]> = await axios.get(
-    "http://localhost:3000/api/pokemons"
+  const allPokemonPromises = Array.from(
+    { length: TOTAL_POKEMON },
+    (_, index) => {
+      const id = index + 1;
+      return Promise.all([
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`),
+        axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
+      ]);
+    }
   );
-  return res.data;
+
+  const allPokemonResponses = await Promise.all(allPokemonPromises);
+
+  const allPokemonData = allPokemonResponses.map(
+    ([response, speciesResponse]) => {
+      const koreanName = speciesResponse.data.names.find(
+        (name: any) => name.language.name === "ko"
+      );
+      return { ...response.data, korean_name: koreanName?.name || null };
+    }
+  );
+
+  return allPokemonData;
 }
 
 export async function getPokemonById(id: string) {
-  const res: AxiosResponse<Pokemon> = await axios.get(
-    `http://localhost:3000/api/pokemons/${id}`
+  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const speciesResponse = await axios.get(
+    `https://pokeapi.co/api/v2/pokemon-species/${id}`
   );
-  return res.data;
+
+  const koreanName = speciesResponse.data.names?.find(
+    (name: any) => name.language.name === "ko"
+  );
+
+  const typesWithKoreanNames = await Promise.all(
+    response.data.types.map(async (type: any) => {
+      const typeResponse = await axios.get(type.type.url);
+      const koreanTypeName =
+        typeResponse.data.names?.find(
+          (name: any) => name.language.name === "ko"
+        )?.name || type.type.name;
+      return { ...type, type: { ...type.type, korean_name: koreanTypeName } };
+    })
+  );
+
+  const abilitiesWithKoreanNames = await Promise.all(
+    response.data.abilities.map(async (ability: any) => {
+      const abilityResponse = await axios.get(ability.ability.url);
+      const koreanAbilityName =
+        abilityResponse.data.names?.find(
+          (name: any) => name.language.name === "ko"
+        )?.name || ability.ability.name;
+      return {
+        ...ability,
+        ability: { ...ability.ability, korean_name: koreanAbilityName },
+      };
+    })
+  );
+
+  const movesWithKoreanNames = await Promise.all(
+    response.data.moves.map(async (move: any) => {
+      const moveResponse = await axios.get(move.move.url);
+      const koreanMoveName =
+        moveResponse.data.names?.find(
+          (name: any) => name.language.name === "ko"
+        )?.name || move.move.name;
+      return { ...move, move: { ...move.move, korean_name: koreanMoveName } };
+    })
+  );
+
+  const pokemonData = {
+    ...response.data,
+    korean_name: koreanName?.name || response.data.name,
+    types: typesWithKoreanNames,
+    abilities: abilitiesWithKoreanNames,
+    moves: movesWithKoreanNames,
+  };
+
+  return pokemonData;
 }
