9522363da8f291b7ca4b9e6ffcb3aa90b0db138a
박원빈 <harry21@kakao.com>
2024-07-03 10:40:22 +0900

    (design) Header: mobile layout

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="fixed top-0 h-24 border-b-[#f0f0f0] border-b w-screen text-white bg-[#EE1515] flex justify-center z-[999]">
-      <div className="max-w-[1224px] w-full flex items-center">
+      <div className="max-w-[1224px] w-full flex items-center px-8">
         <Link href="/" className="flex items-center gap-2">
           <Image
             src="/poketball.png"
